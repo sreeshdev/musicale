a1c7676f63395bd7a7ad5208240894852a6376c3
Sreenaath <sreenaath@192.168.0.106>
2020-11-19 20:52:27 +0530

    deleted unneccessary files

--- src/App/router.js
+++ src/App/router.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Wtf from "../components/wtf";
 import { Router } from "react-router-dom";
 import MainContent from "../components/MainContent";
 import Playlist from "../components/Playlist";
